88204b5aa271eb12aca8d9cf495025b475243fb8
jalipsbio <j.alips@biocoop.fr>
2020-06-18 17:28:58 +0200

    Chap 3.2 - Gérez la navigation avec le Routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,12 @@
-import {Component, OnInit} from '@angular/core';
-import {AppareilService} from './services/appareil.service';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  isAuth = false;
+export class AppComponent {
 
-  appareils: any[];
-
-  lastUpdate = new Promise((resolve, reject) => {
-    const date = new Date();
-    setTimeout(
-      () => {
-        resolve(date);
-      }, 2000
-    );
-  });
-
-  constructor(private appareilService: AppareilService) {
-    setTimeout(
-      () => {
-        this.isAuth = true;
-      }, 4000
-    );
-  }
-
-  ngOnInit() {
-    this.appareils = this.appareilService.appareils;
-  }
-
-  onAllumer() {
-    this.appareilService.switchOnAll();
-  }
-
-  onEteindre() {
-    if(confirm('Etes-vous sûr de vouloir éteindre tous vos appareils ?')) {
-      this.appareilService.switchOffAll();
-    } else {
-      return null;
-    }
-  }
+  constructor() {  }
 
 }
